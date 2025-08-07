bcca44ab20fcdd6775cc514893c6fa51796dbe78
Joseph Taiwo Adebiyi Ogunsanya <159620996+Josephadebiyi@users.noreply.github.com>
2025-08-07 16:10:19 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Global Institute of Tech and Business (GITB) | Practical Online Tech & Business Courses',
+  description: 'GITB offers hands-on tech and business training with globally recognized certifications. Study online, join bootcamps, and get real-world skills employers want. Courses in Cybersecurity, UI/UX, Cloud, KYC, IAM & more.',
+  generator: 'WEBWONDERS.TECH',
 }
 
 export default function RootLayout({
